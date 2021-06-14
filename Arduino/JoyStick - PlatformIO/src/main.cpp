3dc71de02ee8abd7a1f115d860ca6d72c8bcd13f
DanielSurf10 <daniel101surf@gmail.com>
2021-06-13 22:14:19 -0300

    ainda não funciona

--- Arduino/JoyStick - PlatformIO/src/main.cpp
+++ Arduino/JoyStick - PlatformIO/src/main.cpp
@@ -20,7 +20,7 @@ PS2X ps2x;
 // Every analog input is sent as a single channel.
 // Arduino Mega has 16 analog pins, however if your device has fewer you'll need to modify the count and pin list below
 
-#define ANALOG_INPUTS_COUNT 4
+#define ANALOG_INPUTS_COUNT 1
 byte analogPins[] = {PSS_RX, PSS_RY, PSS_LX, PSS_LY}; // element count MUST be == ANALOG_INPUTS_COUNT
 
 
@@ -29,8 +29,8 @@ byte analogPins[] = {PSS_RX, PSS_RY, PSS_LX, PSS_LY}; // element count MUST be =
 // Specify below the digital pin numbers (as for digitalRead) you would like to use.
 // Every pin is sent as a single channel. LOW is encoded as 0, HIGH - as 1023
 // Arduino Mega has 54 digital only pins and the ability to read the analog pins as digital via pin numbers 55-68. If your device has fewer you'll need to modify the count and pin list below
-#define DIGITAL_INPUTS_COUNT 4
-byte digitalPins[] = {PSB_BLUE, PSB_RED, PSB_PINK, PSB_GREEN}; // element count MUST be == DIGITAL_INPUTS_COUNT
+#define DIGITAL_INPUTS_COUNT 0
+bool digitalPins[] = {PSB_L1, PSB_L2, PSB_R1, PSB_R2}; // element count MUST be == DIGITAL_INPUTS_COUNT
 
 
 // 3. Digital bit-mapped channels. Sending a single binary state as a 16-bit
@@ -41,8 +41,8 @@ byte digitalPins[] = {PSB_BLUE, PSB_RED, PSB_PINK, PSB_GREEN}; // element count 
 // The first 16 pins will go in one channel (the first pin goes into the LSB of the channel).
 // The next 16 pins go in another channel and so on
 // LOW pins are encoded as 0 bit, HIGH - as 1.
-#define DIGITAL_BITMAPPED_INPUTS_COUNT 0
-byte digitalBitmappedPins[] = {}; // element count MUST be == DIGITAL_BITMAPPED_INPUTS_COUNT
+#define DIGITAL_BITMAPPED_INPUTS_COUNT 4
+bool digitalBitmappedPins[] = {PSB_L1, PSB_L2, PSB_R1, PSB_R2}; // element count MUST be == DIGITAL_BITMAPPED_INPUTS_COUNT
 
 
 // Define the appropriate analog reference source. See
@@ -66,6 +66,19 @@ IBus ibus(NUM_CHANNELS);
 
 void setup()
 {
+  pinMode(13, OUTPUT);
+
+  if (ps2x.config_gamepad(12, 10, 9, 11, true, true)) { // clock, command, attention, data
+
+    while (1) {
+      digitalWrite(13, HIGH);
+      delay(500);
+      digitalWrite(13, LOW);
+      delay(500);
+    }
+    
+  }
+
   analogReference(ANALOG_REFERENCE); // use the defined ADC reference voltage source
   Serial.begin(BAUD_RATE);           // setup serial
 }
@@ -88,7 +101,7 @@ void loop()
   // read digital bit-mapped pins - 16 pins go in one channel
   for(i=0; i < DIGITAL_BITMAPPED_INPUTS_COUNT; i++) {
   	int bit = i%16;
-  	if(digitalRead(digitalBitmappedPins[i]) == HIGH)
+  	if(ps2x.Button(digitalBitmappedPins[i]) == HIGH)
   		bm_ch |= 1 << bit;
 
   	if(bit == 15 || i == DIGITAL_BITMAPPED_INPUTS_COUNT-1) {
@@ -104,4 +117,5 @@ void loop()
   if(time < UPDATE_INTERVAL)
     // sleep till it is time for the next update
     delay(UPDATE_INTERVAL  - time);
+
 }
